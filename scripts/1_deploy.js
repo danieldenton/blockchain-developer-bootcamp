246e8c109398e679df77cc0d58cda717e904190d
Daniel Denton <danielmdenton@gmail.com>
2024-03-26 16:53:15 -0700

    deploy get token

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,22 +1,11 @@
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Fetch contract to deploy
+  const Token = await ethers.getContractFactory("Token")
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
-
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  // Deploy contreact
 }
+  
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
