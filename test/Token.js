f143d168695061b854a33f308e81544a03c8c974
Daniel Denton <danielmdenton@gmail.com>
2024-03-26 20:28:08 -0700

    tests complete for now

--- test/Token.js
+++ test/Token.js
@@ -5,22 +5,27 @@ const tokens = (n) => {
 	return ethers.utils.parseUnits(n.toString(), "ether")
 }
 
-describe("Token", () => {
 	let token
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory("Token")
 		token = await Token.deploy("Pile Coin", "PC", "1000000")
 	})
-	it("has correct name", async () => { 
-		expect(await token.name()).to.equal("Pile Coin")
+	describe("Deployment", () => {
+		const name = "Pile Coin"
+		const symbol = "PC"
+		const decimals = "18"
+		const totalSupply = tokens(1000000)
+		it("has correct name", async () => { 
+		expect(await token.name()).to.equal(name)
 	})
 	it("has correct symbol", async () => {
-		expect(await token.symbol()).to.equal("PC")
+		expect(await token.symbol()).to.equal(symbol)
 	})
 	it("has correct decimals", async () => {
-		expect(await token.decimals()).to.equal("18")
+		expect(await token.decimals()).to.equal(decimals)
 	})
 	it("has correct total supply", async () => {
-		expect(await token.totalSupply()).to.equal(tokens(1000000))
+		expect(await token.totalSupply()).to.equal(totalSupply)
 	})
+	
 })
