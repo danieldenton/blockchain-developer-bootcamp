5cfab197cc3c7689c799f01bd58bf3e237140e3c
Daniel Denton <danielmdenton@gmail.com>
2024-03-28 17:42:27 -0700

    Transfer event created. Test passes

--- test/Token.js
+++ test/Token.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 
 describe("Token", () => {
-	let token, accounts, deployer
+	let token, accounts, deployer, receiver
 
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory("Token")
@@ -14,28 +14,43 @@ describe("Token", () => {
 
 		accounts = await ethers.getSigners()
 		deployer = accounts[0]
-
+		receiver = accounts[1]
 	})
 	describe("Deployment", () => {
 		const name = "Pile Coin"
 		const symbol = "PC"
 		const decimals = "18"
 		const totalSupply = tokens(1000000)
-		it("has correct name", async () => { 
-		expect(await token.name()).to.equal(name)
-	})
-	it("has correct symbol", async () => {
-		expect(await token.symbol()).to.equal(symbol)
-	})
-	it("has correct decimals", async () => {
-		expect(await token.decimals()).to.equal(decimals)
-	})
-	it("has correct total supply", async () => {
-		expect(await token.totalSupply()).to.equal(totalSupply)
-	})
-    it("assigns total supply to deployer", async () => {
-		expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
-	})
 
+		it("has correct name", async () => { 
+			expect(await token.name()).to.equal(name)
+		})
+		it("has correct symbol", async () => {
+			expect(await token.symbol()).to.equal(symbol)
+		})
+		it("has correct decimals", async () => {
+			expect(await token.decimals()).to.equal(decimals)
+		})
+		it("has correct total supply", async () => {
+			expect(await token.totalSupply()).to.equal(totalSupply)
+		})
+	    it("assigns total supply to deployer", async () => {
+			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+		})
 	})	
-})
+	describe("Transfer", async () => {
+		let amount
+		it("Transfers token balances", async () => {
+			console.log("deployer balance before transfer", await token.balanceOf(deployer.address))
+			console.log("receiver balance before transfer", await token.balanceOf(receiver.address))
+			amount = tokens(100)
+			let transaction  = await token.connect(deployer).transfer(receiver.address, amount)
+			let result = transaction.wait()
+
+			expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+			expect(await token.balanceOf(receiver.address)).to.equal(amount)
+			console.log("deployer balance after transfer", await token.balanceOf(deployer.address))
+			console.log("receiver balance after transfer", await token.balanceOf(receiver.address))
+		})
+	})
+})	
