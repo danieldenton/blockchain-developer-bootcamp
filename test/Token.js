20fdf6d44753c65238dad170dfa72ebc8418308e
Daniel Denton <danielmdenton@gmail.com>
2024-03-28 20:35:06 -0700

    transering tokens

--- test/Token.js
+++ test/Token.js
@@ -38,17 +38,38 @@ describe("Token", () => {
 			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
 		})
 	})	
-	describe("Transfer", async () => {
-		let amount
+	describe("Sending tokens", async () => {
+		let amount, transaction 
 
-		beforeEach(async () => {
+		describe("Success", () => {
+			beforeEach(async () => {
 			amount = tokens(100)
-			let transaction  = await token.connect(deployer).transfer(receiver.address, amount)
-			let result = transaction.wait()
-		})
-		it("Transfers token balances", async () => {
-			expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
-			expect(await token.balanceOf(receiver.address)).to.equal(amount)
+			transaction  = await token.connect(deployer).transfer(receiver.address, amount)
+			result = await transaction.wait()
+			})
+			it("transfers token balances", async () => {
+				expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+				expect(await token.balanceOf(receiver.address)).to.equal(amount)
+			})
+			it("emits a Transfer event", () => {
+				const event = result.events[0]
+				expect(event.event).to.equal("Transfer")
+				const args = event.args
+				expect(args.from).to.equal(deployer.address)
+				expect(args.to).to.equal(receiver.address)
+				expect(args.value).to.equal(amount)
+			})
+
+		})	
+		describe("Failure", () => {
+			it("rejects insufficient balanes", async () => {
+				const invalidAmount = tokens(1000000000)
+				await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+			})
+			it("rejects invalid recipient", async () => {
+				const amount = tokens(100)
+				await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+			})
 		})
 	})
 })	
