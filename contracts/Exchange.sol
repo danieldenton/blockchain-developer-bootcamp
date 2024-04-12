3e73df4da639ddd85ab82955efb1dbc393a33c9c
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-04-12 13:53:59 -0700

    trade function complete no test yet

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -131,4 +131,47 @@ contract Exchange {
 			block.timestamp
 		);
 	}
+
+	function fillOrder(uint256 _id) public {
+		_Order storage _order =  orders[_id];
+
+		_trade(
+			_order.id, 
+			_order.user,
+			_order.tokenGet,
+			_order.amountGet
+			_order.tokenGive,
+			_order.amountGive
+		);
+	}
+
+	function _trade(
+		uint256 _orderId,
+		address _user,
+		address _tokenGet,
+		uint256 _amountGet,
+		address _tokenGive,
+		uint256 _amountGive
+	) internal {
+
+		uint _feeAmount = (amountGet * feePercent) / 100;
+
+		tokens[_tokenGet][msg.sender] = 
+			tokens[_tokenGet][msg.sender] - 
+			(_amountGet + feeAccount);
+		tokens[_tokenGet][_user] = 
+			tokens[_tokenGet][_user] + 
+			_amountGet;
+
+		tokens[_tokenGet][feeAccount] = 
+			tokens[_tokenGet[_user] + 
+			_feeAmount;
+
+		tokens[_tokenGive][_user] = 
+			tokens[_tokenGive][_user] - 
+			_amountGive;
+		tokens[_tokenGive][msg.sender] = 
+			tokens[_tokenGive][msg.sender] + 
+			_amountGive;
+	}
 }
