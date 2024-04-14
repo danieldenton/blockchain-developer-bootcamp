25be495498ff42b23b13ea747ad5a49396ad1a71
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-04-13 17:22:25 -0700

    deplly script complete

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,12 +1,31 @@
 
 async function main() {
+  console.log(`Preparing deployment...\n`)
+
   // Fetch contract to deploy
   const Token = await ethers.getContractFactory("Token")
+  const Exchange = await ethers.getContractFactory("Exchange")
 
-  // Deploy contreact
-  const token = await Token.deploy()
-  await token.deployed()
-  console.log(`Token deployed to: ${token.address}`)
+  const accounts = await ethers.getSigners()
+
+  console.log(`Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`)
+
+  // Deploy contract
+  const pilecoin = await Token.deploy("Pile Coin", "PC", "1000000")
+  await pilecoin.deployed()
+  console.log(`Token deployed to: ${pilecoin.address}`)
+
+  const mETH = await Token.deploy("mETH", "mETH", "1000000")
+  await mETH.deployed()
+  console.log(`Token deployed to: ${mETH.address}`)
+
+  const mDAI = await Token.deploy("mDAI", "mDAI", "1000000")
+  await mDAI.deployed()
+  console.log(`Token deployed to: ${mDAI.address}`)
+
+  const exchange = await Exchange.deploy(accounts[1], 10)
+  await exchange.deployed(`Echange deployed to: ${exchange.address}`)
+  console.log()
 }
   
 
